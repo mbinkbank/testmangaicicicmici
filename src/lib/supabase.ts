aa44607a8eac06963e3f505c8a0c7d7aa94f41fd
mbinkbank <35717157+mbinkbank@users.noreply.github.com>
2026-07-15 15:19:24 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://btbgkxhicdbdzqwcdlgd.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0YmdreGhpY2RiZHpxd2NkbGdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5MjE0MDAsImV4cCI6MjA1MTQ5NzQwMH0.Ej8vKqL5nX8mP3qR7sT9uV1wY2zA4bC6dE8fG0hI2jK4";
+const supabaseAnonKey = "sb_publishable_VMGnQuP0jWqA61s6DgPFgA__GXkkpIm";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
